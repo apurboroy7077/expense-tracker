34ebb5d068a5f6659d1c91683ae03310328f77db
Apurbo Roy <apurboroy7077@gmail.com>
2024-09-16 07:46:10 +0600

    added search result section for smaller screen in reports page

---
+++ js/reports-page/scriptsOfReportsPage.js
@@ -0,0 +1,8 @@
+import navbarMainScript from "../navbar/main/navbarMainScript.js";
+
+const scriptsOfReportsPage = () => {
+  navbarMainScript();
+};
+scriptsOfReportsPage();
+
+export default scriptsOfReportsPage;

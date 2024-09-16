aa2c3dc13bae461a48dec50ba8c32ad79c619738
Apurbo Roy <apurboroy7077@gmail.com>
2024-09-16 09:31:23 +0600

    added leaderboard page.

---
+++ js/leaderboard-page-script/scriptsOfLeaderboardPage.js
@@ -0,0 +1,8 @@
+import navbarMainScript from "../navbar/main/navbarMainScript.js";
+
+const scriptsOfLeaderboardPage = () => {
+  navbarMainScript();
+};
+
+scriptsOfLeaderboardPage();
+export default scriptsOfLeaderboardPage;
